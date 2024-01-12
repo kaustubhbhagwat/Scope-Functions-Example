65f86b36a524199e6fc8bf55ae1a2fa875e37378
Kaustubh <kaustubhbhagwat93@gmail.com>
2024-01-12 13:58:27 +0530

    Initial commit

---
+++ src/main/kotlin/ScopeFunctionWith.kt
@@ -0,0 +1,24 @@
+private class Person {
+    var name: String? = null
+    var age: Int = 30
+}
+
+
+fun main() {
+    /** Scope function 'with'
+        Property 1: Refer to the context of the object by using 'this'
+        Property 1: Return value is 'lambda result'
+     */
+
+    val person = Person()
+   val ageAfter5years = with(person) {
+        this.age = 30
+        this.name = "Kaustubh"
+
+        println(name)
+        println(age)
+        age + 5
+       "Learning Scope functions"
+    }
+    println("Currently $ageAfter5years")
+}
